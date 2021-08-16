e325ed6df9d5357f89c41610d63e83c1cf2851b1
ridasilva <ridasilva@usp.br>
2021-08-16 14:00:57 -0300

    adding setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='regFilter',
+    version='0.0.1',
+    description='Fiter mass features with linear regression',
+    license='BSD-3-Clause',
+    packages=['regFilter'],
+    author='Ricardo R. da Silva',
+    scripts=['bin/reg_filter'],
+    author_email='ridasilva@usp.br'
+)
